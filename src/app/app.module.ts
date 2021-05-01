e325a0b014fa405a18c64d2edb368eb715b2ec89
Akkineni Pavan <pavanchowdary.akkineni@gmail.com>
2021-05-01 12:31:38 +0530

    promises part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
@@ -57,6 +58,7 @@ import { LoginComponent } from './login/login.component';
     MatGridListModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
